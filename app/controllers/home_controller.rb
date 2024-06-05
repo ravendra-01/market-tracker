a6190e69f2ee5db0978faecaef3ecc4e34bd8ba3
rkpatel <rk.ravendra01@gmail.com>
2024-06-05 17:22:42 +0530

    configured tailwindcss

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# HomeController is responsible for handling requests to the home page of the application.
+# This controller currently has a single action, `index`, which renders the home page view.
+class HomeController < ApplicationController
+  def index; end
+end
